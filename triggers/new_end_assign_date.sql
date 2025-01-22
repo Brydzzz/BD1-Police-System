9250b67a6d567456756c993902e01a09517450c0
Kornelia Błaszczuk <kornelia.blaszczuk@onet.pl>
2025-01-22 21:02:59 +0100

    final thankfully working

--- triggers/new_end_assign_date.sql
+++ triggers/new_end_assign_date.sql
@@ -1,27 +1,30 @@
-create or replace TRIGGER new_end_assign_date
-    before insert or update of END_ASSIGN_DATE
-    on ASSIGNED_CASE
-    for each row
+CREATE OR REPLACE TRIGGER new_end_assign_date
+    BEFORE INSERT OR UPDATE OF END_ASSIGN_DATE
+    ON ASSIGNED_CASE
+    FOR EACH ROW
 BEGIN
-    IF :NEW.end_assign_date IS NULL THEN
+    -- Check if the new END_ASSIGN_DATE is NULL
+    IF :NEW.END_ASSIGN_DATE IS NULL THEN
         RETURN;
     END IF;
 
-    IF :NEW.end_assign_date > SYSDATE THEN
+    -- Check if END_ASSIGN_DATE is in the future
+    IF :NEW.END_ASSIGN_DATE > SYSDATE THEN
         RAISE_APPLICATION_ERROR(-20001, 'Data zakończenia sprawy nie może być w przyszłości.');
     END IF;
 
-    IF :NEW.end_assign_date < :new.start_assign_date then
-        RAISE_APPLICATION_ERROR(-20002, 'Data zakończenia sprawy musi być późniejsza niż data zakończenia sprawy');
+    -- Check if END_ASSIGN_DATE is before START_ASSIGN_DATE
+    IF :NEW.END_ASSIGN_DATE < :NEW.START_ASSIGN_DATE THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Data zakończenia sprawy musi być późniejsza niż data rozpoczęcia sprawy.');
     END IF;
-
-end;
+END;
+/
 
 
 -- assign date zmieniona na w przeszłości, brak błędu
 UPDATE ASSIGNED_CASE
 SET END_ASSIGN_DATE = TO_DATE('2025-01-13', 'YYYY-MM-DD')
-WHERE ASSIGN_ID = 103;
+WHERE ASSIGN_ID = 104;
 
 -- NULL scenario
 UPDATE ASSIGNED_CASE
